719fe033eb3f90b4fce5fbeff73e6eafbaba772a
hongminyeong <minyung1240@daum.net>
2024-02-06 17:03:31 +0900

    study: AppModule에 TypeORM과 DB정보 등록하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,39 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+// dotenv 패키지 직접 사용
+dotenv.config({
+  path: path.resolve(
+    process.env.NODE_ENV === 'dev'
+      ? '.dev.env'
+      : process.env.NODE_ENV === 'stage'
+        ? '.stage.env'
+        : '.local.env',
+  ),
+});
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        retryAttempts: 10,
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        database: 'study',
+        username: 'root',
+        password: process.env.DATABASE_PASSWORD,
+        entities: [path.join(__dirname, '/entities/**/*.entity.{js,ts}')],
+        synchronize: false, // 무조건 false 로 해두기.
+        logging: true, // typeorm 쿼리 실행시, MySQL의 쿼리문을 터미널에 보여줍니다.
+        timezone: 'local',
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
